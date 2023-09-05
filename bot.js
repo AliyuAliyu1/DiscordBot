9058daab183381a4ca9df778d5904245c3968783
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2023-09-05 13:36:18 +0100

    updated with slash

--- bot.js
+++ bot.js
@@ -1,32 +1,61 @@
 import { config } from "dotenv";
-import { REST, Routes } from 'discord.js';
-import { Client, GatewayIntentBits} from 'discord.js';
+import { Client, GatewayIntentBits, Routes } from "discord.js";
+import { REST } from "@discordjs/rest";
+// starting the config for hiding the Token
 config();
-const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMessages] });
 
-const TOKEN = process.env.Token_hider
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+//  creating a variable holder for the token imported from the .env file
+const TOKEN = process.env.Token_hider;
 
-client.login(TOKEN)
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
-  client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-  
-    if (interaction.commandName === 'ping') {
-      await interaction.reply('Pong!');
-    }
-  });
+// creating a variable holders for the Slash command imported from the .env file
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+// creating a client object and also a message content holder to hold the messages
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
 
-  client.on('messageCreate', (message) => {
-    console.log(message.content);
-    console.log(message.createdAt.toDateString())
-    console.log(message.author.tag)
-    if (message.author.bot){
-        return;
-    }
-     if(message.content === 'hello'){
-        return message.reply('guy dat your car you fit dash me...');
-    }
-  });
+// starting the Rest to interact with the api using the token
+
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+// listens for any user input
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// once a message has been logged in it will take the message and reply with a certain messsage
+client.on("messageCreate", (message) => {
+  console.log(message.content);
+  console.log(message.createdAt.toDateString());
+  console.log(message.author.tag);
+  if (message.author.bot) {
+    return;
+  }
+  if (message.content === "hello") {
+    return message.reply("guy dat your car you fit dash me...");
+  }
+});
+
+//  creating the default log in slash object
+async function main() {
+  const commands = [
+    {
+      name: "jokes",
+      description: "produces random jokes",
+    },
+  ];
+  try {
+    console.log("started refreshing application (/) command.");
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    client.login(TOKEN);
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
